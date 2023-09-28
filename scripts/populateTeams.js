e18f2c202700185329c3036c049b7d4e977e88a1
Jean Rabelo <jrabelo75@hotmail.com>
2023-09-28 15:17:45 -0300

    add leagues

--- scripts/populateTeams.js
+++ scripts/populateTeams.js
@@ -1,60 +1,66 @@
-import axios from 'axios';
-import mongoose from 'mongoose';
-import { config } from 'dotenv';
-import Team from '../src/models/team.js';
-import League from '../src/models/league.js';
+import axios from "axios";
+import mongoose from "mongoose";
+import { config } from "dotenv";
+import leagues from './leagues.json';
+import Team from "../src/models/team.js";
+import League from "../src/models/league.js";
 
 config();
 
-const API_FOOTBALL_KEY = process.env.API_FOOTBALL_KEY || '';
-const API_FOOTBALL_HOST = process.env.API_FOOTBALL_HOST || '';
-const API_FOOTBALL_URL = process.env.API_FOOTBALL_URL || '';
+const API_FOOTBALL_KEY = process.env.API_FOOTBALL_KEY || "";
+const API_FOOTBALL_HOST = process.env.API_FOOTBALL_HOST || "";
+const API_FOOTBALL_URL = process.env.API_FOOTBALL_URL || "";
 
 async function populateTeams() {
   try {
     if (!API_FOOTBALL_KEY || !API_FOOTBALL_HOST || !API_FOOTBALL_URL) {
-      throw new Error('As configurações da API não foram encontradas.');
+      throw new Error("As configurações da API não foram encontradas.");
     }
 
-    const response = await axios.get(API_FOOTBALL_URL + 'teams', {
-      headers: {
-        'X-RapidAPI-Key': API_FOOTBALL_KEY,
-        'X-RapidAPI-Host': API_FOOTBALL_HOST
-      },
-      params: {
-        league: '135',
-        season: '2023'
-      }
-    });
+    for (const entry of leagues.leagues) {
+      const { name, country, code, number } = entry;
+      const season = "2023";
 
-    console.log('Status da resposta da API:', response.status);
-
-    const newLeague = new League({
-      name: "Serie A",
-      country: "Italy",
-      code: "ITA1",
-    });
-
-    newLeague.save();
-
-    for (const entry of response.data.response) {
-      const { team, venue } = entry;
-
-      const newTeam = new Team({
-        name: team.name,
-        country: team.country,
-        league: newLeague._id,
-        code: team.code,
-        founded: team.founded,
-        logo: team.logo,
-        city: venue.city
+      const response = await axios.get(API_FOOTBALL_URL + "teams", {
+        headers: {
+          "X-RapidAPI-Key": API_FOOTBALL_KEY,
+          "X-RapidAPI-Host": API_FOOTBALL_HOST,
+        },
+        params: {
+          league: number,
+          season: season,
+        },
       });
 
-      try {
-        await newTeam.save();
-        console.log('Time ' + team.name + ' salvo!');
-      } catch (error) {
-        console.error('Erro ao salvar o time ' + team.name + ':', error);
+      console.log("Status da resposta da API:", response.status);
+
+      const newLeague = new League({
+        name: name,
+        country: country,
+        code: code,
+      });
+
+      newLeague.save();
+
+      for (const entry of response.data.response) {
+        const { team, venue } = entry;
+
+        const newTeam = new Team({
+          name: team.name,
+          country: team.country,
+          league: newLeague._id,
+          code: team.code,
+          founded: team.founded,
+          logo: team.logo,
+          city: venue.city,
+        });
+
+        try {
+          await newTeam.save();
+          console.log("Time " + team.name + " salvo!");
+        } catch (error) {
+          console.error("Erro ao salvar o time " + team.name + ":", error);
+        }
       }
     }
 
@@ -66,12 +72,12 @@ async function populateTeams() {
   }
 }
 
-mongoose.connect('mongodb://localhost:27017/spotlightplayers', {
+mongoose.connect("mongodb://localhost:27017/spotlightplayers", {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
-mongoose.connection.once('open', () => {
-  console.log('Conexão com o MongoDB estabelecida com sucesso!');
+mongoose.connection.once("open", () => {
+  console.log("Conexão com o MongoDB estabelecida com sucesso!");
   populateTeams();
 });
