fb3620dcd952452cd82310c18bbb9d8ed90828da
Jean Rabelo <jrabelo75@hotmail.com>
2023-09-29 16:29:38 -0300

    aws config file

---
+++ src/config/aws.js
@@ -0,0 +1,11 @@
+import AWS from 'aws-sdk';
+
+const ACCESS_KEY_ID = process.env.ACCESS_KEY_ID || "";
+const SECRET_ACCESS_KEY = process.env.SECRET_ACCESS_KEY || "";
+
+const s3 = new AWS.S3({
+  accessKeyId: ACCESS_KEY_ID,
+  secretAccessKey: SECRET_ACCESS_KEY,
+});
+
+module.exports = s3;
